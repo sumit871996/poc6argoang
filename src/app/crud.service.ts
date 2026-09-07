7848f2b68dfb30096716c3f66975040d6f1961e7
sumit871996 <107835501+sumit871996@users.noreply.github.com>
2026-09-07 22:44:25 +0530

    Update crud.service.ts

--- src/app/crud.service.ts
+++ src/app/crud.service.ts
@@ -11,7 +11,7 @@ export class CrudService {
   constructor(private http: HttpClient) { }
 
   public getpublicdata(): Observable<any> {
-    return this.http.get("http://localhost:28962/api/User/Public", { responseType: 'text' }).pipe(
+    return this.http.get("http://10.3.17.126:28962/api/User/Public", { responseType: 'text' }).pipe(
       catchError(this.handleError)
     );
   }
